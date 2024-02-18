fbb83d9811c452f773f45ae6fbcc1dfd6c632bea
u5144821 <anna.nadolny@anu.edu.au>
2024-02-18 19:24:59 +1100

    Added rooftop solar function

--- run.bat
+++ run.bat
@@ -5,4 +5,16 @@ python Optimisation.py -e 3 -i 200 -p 8 -r 0.4 -n VH
 python Optimisation.py -e 3 -i 200 -p 8 -r 0.4 -n LA
 python Optimisation.py -e 3 -i 200 -p 8 -r 0.4 -n KH
 python Optimisation.py -e 3 -i 200 -p 8 -r 0.4 -n TH
+python Optimisation.py -e 3 -i 400 -p 8 -r 0.4
+python Optimisation.py -e 6 -i 200 -p 8 -r 0.4 -n VS
+python Optimisation.py -e 6 -i 200 -p 8 -r 0.4 -n VH
+python Optimisation.py -e 6 -i 200 -p 8 -r 0.4 -n LA
+python Optimisation.py -e 6 -i 200 -p 8 -r 0.4 -n KH
+python Optimisation.py -e 6 -i 200 -p 8 -r 0.4 -n TH
 python Optimisation.py -e 6 -i 400 -p 8 -r 0.4
+python Optimisation.py -e 9 -i 200 -p 8 -r 0.4 -n VS
+python Optimisation.py -e 9 -i 200 -p 8 -r 0.4 -n VH
+python Optimisation.py -e 9 -i 200 -p 8 -r 0.4 -n LA
+python Optimisation.py -e 9 -i 200 -p 8 -r 0.4 -n KH
+python Optimisation.py -e 9 -i 200 -p 8 -r 0.4 -n TH
+python Optimisation.py -e 9 -i 400 -p 8 -r 0.4
