2c1720407b39802079591059d0fe75d9498b1be8
AnnaSN <annasrnadolny@gmail.com>
2024-02-21 00:28:52 +1100

    Minor

--- run.bat
+++ run.bat
@@ -1,8 +1,10 @@
 @echo off
 
-
-python Optimisation.py -e 73 -i 189 -p 8 -r 0.4 -n KH
-python Optimisation.py -e 76 -i 189 -p 8 -r 0.4 -n KH
-python Optimisation.py -e 76 -i 400 -p 8 -r 0.4
-python Optimisation.py -e 79 -i 189 -p 8 -r 0.4 -n KH
-python Optimisation.py -e 79 -i 400 -p 8 -r 0.4
+python Optimisation.py -e 3 -i 300 -p 8 -r 0.4
+python Optimisation.py -e 6 -i 300 -p 8 -r 0.4
+python Optimisation.py -e 9 -i 300 -p 8 -r 0.4
+python Optimisation.py -e 73 -i 300 -p 8 -r 0.4
+python Optimisation.py -e 83 -i 300 -p 8 -r 0.4
+python Optimisation.py -e 93 -i 300 -p 8 -r 0.4
+python Optimisation.py -e 83 -i 300 -p 8 -r 0.4
+python Optimisation.py -e 93 -i 300 -p 8 -r 0.4
